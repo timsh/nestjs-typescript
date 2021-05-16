8858d7111c95acfc8b52fc10816b79751420ea24
timsh <timmy.shen@gmail.com>
2021-05-16 14:32:03 +0800

    Initialize part-20

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn, Index } from 'typeorm';
+import User from '../users/user.entity';
+import Category from '../categories/category.entity';
+
+@Entity()
+class Post {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public title: string;
+
+  @Column('text', { array: true })
+  public paragraphs: string[];
+
+  @Column({ nullable: true })
+  public category?: string;
+
+  @Index('post_authorId_index')
+  @ManyToOne(() => User, (author: User) => author.posts)
+  public author: User;
+
+  @ManyToMany(() => Category, (category: Category) => category.posts)
+  @JoinTable()
+  public categories: Category[];
+}
+
+export default Post;
