c880907e967a287a993a01f508fbd3bc40a70120
timsh <timmy.shen@gmail.com>
2021-05-16 14:31:56 +0800

    Initialize part-5

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+class Post {
+  @PrimaryGeneratedColumn()
+  public id?: number;
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public content: string;
+
+  @Column({ nullable: true })
+  public category?: string;
+}
+
+export default Post;
