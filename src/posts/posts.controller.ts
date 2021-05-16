9c4b8c68d8ca68fdf9dfcd619ac8119f65c4ec72
timsh <timmy.shen@gmail.com>
2021-05-16 14:32:03 +0800

    Initialize part-19

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+  Req,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Query,
+} from '@nestjs/common';
+import PostsService from './posts.service';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import FindOneParams from '../utils/findOneParams';
+import RequestWithUser from '../authentication/requestWithUser.interface';
+import { PaginationParams } from '../utils/types/paginationParams';
+
+@Controller('posts')
+@UseInterceptors(ClassSerializerInterceptor)
+export default class PostsController {
+  constructor(
+    private readonly postsService: PostsService
+  ) {}
+
+  @Get()
+  async getPosts(
+    @Query('search') search: string,
+    @Query() { offset, limit, startId }: PaginationParams
+  ) {
+    if (search) {
+      return this.postsService.searchForPosts(search, offset, limit, startId);
+    }
+    return this.postsService.getAllPosts(offset, limit, startId);
+  }
+
+  @Get(':id')
+  getPostById(@Param() { id }: FindOneParams) {
+    return this.postsService.getPostById(Number(id));
+  }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  async createPost(@Body() post: CreatePostDto, @Req() req: RequestWithUser) {
+    return this.postsService.createPost(post, req.user);
+  }
+
+  @Patch(':id')
+  async updatePost(@Param() { id }: FindOneParams, @Body() post: UpdatePostDto) {
+    return this.postsService.updatePost(Number(id), post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param() { id }: FindOneParams) {
+    return this.postsService.deletePost(Number(id));
+  }
+}
