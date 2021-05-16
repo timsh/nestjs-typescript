b872f5b17cd6c3ef894c4bc84964838f94764320
timsh <timmy.shen@gmail.com>
2021-05-16 14:31:56 +0800

    Initialize part-4

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import PostsService from './posts.service';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import FindOneParams from '../utils/findOneParams';
+
+@Controller('posts')
+export default class PostsController {
+  constructor(
+    private readonly postsService: PostsService
+  ) {}
+
+  @Get()
+  getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(':id')
+  getPostById(@Param() { id }: FindOneParams) {
+    return this.postsService.getPostById(Number(id));
+  }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  async createPost(@Body() post: CreatePostDto) {
+    return this.postsService.createPost(post);
+  }
+
+  @Patch(':id')
+  async updatePost(@Param() { id }: FindOneParams, @Body() post: UpdatePostDto) {
+    return this.postsService.updatePost(Number(id), post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param() { id }: FindOneParams) {
+    return this.postsService.deletePost(Number(id));
+  }
+}
