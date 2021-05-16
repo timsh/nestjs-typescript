a6425b28d5394116a84328e03f05b4faa718ea03
timsh <timmy.shen@gmail.com>
2021-05-16 14:31:57 +0800

    Initialize part-8

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { ExcludeNullInterceptor } from './utils/excludeNull.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ExcludeNullInterceptor());
+  app.use(cookieParser());
+  await app.listen(3000);
+}
+bootstrap();
